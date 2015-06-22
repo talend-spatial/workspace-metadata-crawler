c1e5ed26500dc61bf6a2e5f01fde446464fdfa40
François Prunayre <fxprunayre@gmail.com>
2015-06-22 13:28:12 +0200

    Rename _resources folder to config. Fix script for overview #21.

--- METADATA-CRAWLER/config/create_overview.bat
+++ METADATA-CRAWLER/config/create_overview.bat
@@ -1,4 +1,4 @@
 echo "Creating overview for %1 (layer %2) ..."
-gdal_rasterize -ts %5 %6 -ot Byte -burn 111 %2 "%1" "%3"
+gdal_rasterize -ts %5 %6 -ot Byte -burn 111 -l %2 "%1" "%3"
 gdal_translate -a_nodata 0 -of PNG -outsize %5 %6 -b 1 "%3" "%4"
 echo "Done."
